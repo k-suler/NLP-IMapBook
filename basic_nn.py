3031d33a2d55cb0c31e7724c432c32c6eae3011d
k-suler <kristijan.suler@gmail.com>
2021-05-02 17:24:51 +0200

    Change: refactor

--- basic_nn.py
+++ basic_nn.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras.layers import Activation, Dense
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import LabelEncoder
 from tensorflow import keras
@@ -15,8 +15,172 @@ from evaluation import Evaluator
 from feature_extraction import bag_of_words_features
 from preprocess import preprocess_data
 from utils import get_classes, preprocess_labels, split_train_test
+from keras.utils import plot_model
 
-tfidf = TfidfVectorizer(binary=True, stop_words="english")
+tfidf = TfidfVectorizer(binary=True, stop_words="english", max_df=0.5, min_df=2)
+
+
+class NN:
+    def __init__(self, type, labelEncoder):
+        self.type = type
+        self.model = None
+        self.lb = labelEncoder
+
+    def set_train_model(self, type, nb_classes, dims):
+        self.type = type
+
+        if self.type == "basic":
+            model = keras.Sequential()
+            model.add(Dense(nb_classes, input_shape=(dims,)))
+            model.add(Activation("softmax"))
+            model.compile(
+                optimizer="sgd", loss="categorical_crossentropy", metrics=["accuracy"]
+            )
+        elif self.type == "deep":
+            model = keras.Sequential()
+            model.add(
+                Dense(
+                    nb_classes,
+                    input_shape=(dims,),
+                    # kernel_initializer=keras.initializers.he_normal(seed=1),
+                    # activation="relu",
+                )
+            )
+            model.add(keras.layers.Dropout(0.71))
+            model.add(
+                keras.layers.Dense(
+                    400,
+                    kernel_initializer=keras.initializers.he_normal(seed=2),
+                    activation="relu",
+                )
+            )
+            model.add(keras.layers.Dropout(0.71))
+            model.add(
+                keras.layers.Dense(
+                    nb_classes,
+                    kernel_initializer=keras.initializers.RandomNormal(
+                        mean=0.0, stddev=0.05, seed=5
+                    ),
+                    activation="softmax",
+                )
+            )
+            model.compile(
+                optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"]
+            )
+        elif self.type == "lstm":
+            model = keras.Sequential()
+            # model.add(
+            #     Dense(
+            #         nb_classes,
+            #         input_shape=(dims,),
+            #         # kernel_initializer=keras.initializers.he_normal(seed=1),
+            #         # activation="relu",
+            #     )
+            # )
+            model.add(
+                keras.layers.LSTM(
+                    nb_classes,
+                    activation="relu",
+                    kernel_initializer="he_normal",
+                    input_shape=(dims, 1),
+                )
+            )
+            model.add(
+                keras.layers.Dense(
+                    10, activation="relu", kernel_initializer="he_normal"
+                )
+            )
+            model.add(
+                keras.layers.Dense(8, activation="relu", kernel_initializer="he_normal")
+            )
+            model.add(
+                keras.layers.Dense(
+                    nb_classes,
+                    kernel_initializer=keras.initializers.RandomNormal(
+                        mean=0.0, stddev=0.05, seed=4
+                    ),
+                    activation="softmax",
+                )
+            )
+
+            model.compile(
+                optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"]
+            )
+        else:
+            raise "Unsupported model type"
+        self.model = model
+
+    def set_type(self, type):
+        self.type
+
+    def train(
+        self,
+        X_train,
+        Y_train,
+        type="deep",
+        X_validation=None,
+        Y_validation=None,
+        save_model=True,
+        filename="model",
+    ):
+
+        nb_classes = Y_train.shape[1]
+        dims = X_train.shape[1]
+        self.set_train_model(self.type, nb_classes, dims)
+
+        fBestModel = f"{filename}-{self.type}.h5"
+        early_stop = EarlyStopping(monitor="val_loss", patience=2, verbose=1)
+        best_model = ModelCheckpoint(fBestModel, verbose=0, save_best_only=True)
+
+        history = self.model.fit(
+            X_train,
+            Y_train,
+            epochs=500,
+            batch_size=1024,
+            validation_data=(X_test, Y_test),
+            callbacks=[best_model, early_stop],
+            verbose=True,
+        )
+        self.model.save(fBestModel)
+        self.model.summary()
+
+        plt.plot(history.history["accuracy"])
+        plt.plot(history.history["val_accuracy"])
+        plt.title("model accuracy")
+        plt.ylabel("accuracy")
+        plt.xlabel("epoch")
+        plt.legend(["train", "test"], loc="upper left")
+        plt.show()
+        # summarize history for loss
+        plt.plot(history.history["loss"])
+        plt.plot(history.history["val_loss"])
+        plt.title("model loss")
+        plt.ylabel("loss")
+        plt.xlabel("epoch")
+        plt.legend(["train", "test"], loc="upper left")
+        plt.show()
+
+    def load_fitted_model(self, filename):
+        self.model = load_model(filename)
+
+    def evaluate(self, X_test, Y_test):
+        if not self.model:
+            raise "Load or fit new model first"
+
+        score, acc = self.model.evaluate(X_test, Y_test, batch_size=3)
+        print("Test accuracy:", acc)
+
+        evaluator = Evaluator()
+        predictions_encoded = self.model.predict(X_test)
+        predictions = self.lb.inverse_transform(
+            [np.argmax(pred) for pred in predictions_encoded]
+        )
+        evaluator.accuracy(Y_test_classes, predictions)
+        evaluator.classification_report(Y_test_classes, predictions)
+        evaluator.confusion_matrix(Y_test_classes, predictions)
+
+    def plot_model(self):
+        plot_model(self.model, to_file="model.png", show_shapes=True)
 
 
 def tfidf_features(txt, flag):
@@ -28,127 +192,39 @@ def tfidf_features(txt, flag):
     return x
 
 
-use_tfidf = True
+if __name__ == "__main__":
+    data = preprocess_data()
+    X_train, X_test, Y_train, Y_test = split_train_test(data, x_col="joined_lemas")
+    Y_test_classes = Y_test
 
-data = preprocess_data()
-X_train, X_test, Y_train, Y_test = split_train_test(data, x_col="joined_lemas")
-Y_test_classes = Y_test
+    lb = LabelEncoder()
+    lb.fit(get_classes(data).tolist())
+    Y_train = lb.transform(Y_train["CodePreliminary"].tolist())
+    Y_train = keras.utils.to_categorical(Y_train)
+    Y_test = lb.transform(Y_test["CodePreliminary"].tolist())
+    Y_test = keras.utils.to_categorical(Y_test)
 
-if use_tfidf:
-    X_train = tfidf_features(X_train["joined_lemas"].tolist(), flag="train")
-    X_test = tfidf_features(X_test["joined_lemas"].tolist(), flag="test")
-else:
-    X_train, X_test = bag_of_words_features(
-        X_train["joined_lemas"].tolist(), X_test["joined_lemas"].tolist(), binary=True
-    )
+    use_tfidf = False
+    if use_tfidf:
+        X_train = tfidf_features(X_train["joined_lemas"].tolist(), flag="train")
+        X_test = tfidf_features(X_test["joined_lemas"].tolist(), flag="test")
 
-lb = LabelEncoder()
-lb.fit(get_classes(data).tolist())
-Y_train = lb.transform(Y_train["CodePreliminary"].tolist())
-Y_train = keras.utils.to_categorical(Y_train)
-Y_test = lb.transform(Y_test["CodePreliminary"].tolist())
-Y_test = keras.utils.to_categorical(Y_test)
-
-
-nb_classes = Y_train.shape[1]
-dims = X_train.shape[1]
-
-
-simple = False
-if simple:
-    model = keras.Sequential()
-    model.add(Dense(nb_classes, input_shape=(dims,)))
-    model.add(Activation("softmax"))
-    model.compile(
-        optimizer="sgd", loss="categorical_crossentropy", metrics=["accuracy"]
-    )
-else:
-    model = keras.Sequential()
-    model.add(
-        Dense(
-            nb_classes,
-            input_shape=(dims,),
-            # kernel_initializer=keras.initializers.he_normal(seed=1),
-            # activation="relu",
+        # X_train = X_train[:, :, None]
+        # X_test = X_test[:, :, None]
+        # X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], 1))
+        # X_test = X_test.reshape((X_test.shape[0], X_test.shape[1], 1))
+    else:
+        X_train, X_test = bag_of_words_features(
+            X_train["joined_lemas"].tolist(),
+            X_test["joined_lemas"].tolist(),
+            binary=True,
         )
-    )
-    model.add(keras.layers.Dropout(0.71))
-    model.add(
-        keras.layers.Dense(
-            400,
-            kernel_initializer=keras.initializers.he_normal(seed=2),
-            activation="relu",
-        )
-    )
-    model.add(keras.layers.Dropout(0.71))
-    model.add(
-        keras.layers.Dense(
-            nb_classes,
-            kernel_initializer=keras.initializers.RandomNormal(
-                mean=0.0, stddev=0.05, seed=4
-            ),
-            activation="softmax",
-        )
-    )
-    model.compile(
-        optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"]
-    )
 
-fBestModel = "best_model.h5"
-early_stop = EarlyStopping(monitor="val_loss", patience=8, verbose=1)
-best_model = ModelCheckpoint(fBestModel, verbose=0, save_best_only=True)
-
-
-history = model.fit(
-    X_train,
-    Y_train,
-    epochs=3000,
-    batch_size=1024,
-    validation_data=(X_test, Y_test),
-    callbacks=[best_model, early_stop],
-    verbose=True,
-)
-print(best_model)
-
-model.save_weights("model.h5")
-print("Saved model to disk")
-
-score, acc = model.evaluate(X_test, Y_test, batch_size=3)
-print("Test accuracy:", acc)
-
-
-print(history.history.keys())
-plt.plot(history.history["accuracy"])
-plt.plot(history.history["val_accuracy"])
-plt.title("model accuracy")
-plt.ylabel("accuracy")
-plt.xlabel("epoch")
-plt.legend(["train", "test"], loc="upper left")
-plt.show()
-# summarize history for loss
-plt.plot(history.history["loss"])
-plt.plot(history.history["val_loss"])
-plt.title("model loss")
-plt.ylabel("loss")
-plt.xlabel("epoch")
-plt.legend(["train", "test"], loc="upper left")
-plt.show()
-
-
-print("\nModel description:")
-model.summary()
-
-
-evaluator = Evaluator()
-predictions_encoded = model.predict(X_test)
-predictions = lb.inverse_transform([np.argmax(pred) for pred in predictions_encoded])
-evaluator.accuracy(Y_test_classes, predictions)
-evaluator.classification_report(Y_test_classes, predictions)
-evaluator.confusion_matrix(Y_test_classes, predictions)
-
-
-# from IPython.display import SVG
-# from keras.utils.vis_utils import model_to_dot
+    nn = NN("deep", lb)
+    # nn.train(X_train, Y_train)
+    nn.load_fitted_model("./model-deep.h5")
+    nn.evaluate(X_test, Y_test)
+    nn.plot_model()
 
 # Graphical
 # SVG(model_to_dot(model).create(prog="dot", format="svg"))
