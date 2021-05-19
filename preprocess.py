219b4f8200b1dd2efd8a8075fcb2d2158cd239d2
MihaArh <ma6809@student.uni-lj.si>
2021-05-19 18:43:08 +0200

    Change: additional text cleanup

--- preprocess.py
+++ preprocess.py
@@ -2,6 +2,7 @@ import pandas as pd
 import nltk
 import string
 import contractions
+from nltk.corpus import stopwords
 
 from constants import emoticons
 
@@ -18,8 +19,6 @@ def preprocess_data():
     df = read_crew_data()
     lemmatizer = nltk.stem.WordNetLemmatizer()
 
-
-
     # df['lemas'] = (
     #     df["Message"]
     #     .str.lower()
@@ -33,19 +32,51 @@ def preprocess_data():
     #     .apply(lambda tokens: [lemmatizer.lemmatize(token) for token in tokens])
     # )
 
-    df["tokens"] = df["Message"].str.lower().apply(contractions.fix).str.split(" ")
-    df["tokens"] = df["tokens"].apply(
-        lambda tokens: [
-            x.translate(str.maketrans({key: None for key in string.punctuation}))
-            if x not in emoticons
-            else x
-            for x in tokens
-        ]
+    def check_for_dots(tokens):
+        if len(tokens) > 1:
+            return list(map(lambda token: token.replace(".", ""), tokens))
+        return tokens
+
+    df["Message"] = (
+        df["Message"]
+        .str.strip()
+        .str.replace("\n", " ")
+        .str.replace("\r", " ")
+        .str.replace("’", "'")
+        .str.replace("…", "...")
+        .str.replace("“", "'")
+        .str.replace("—", " ")
     )
+    df["tokens"] = (
+        df["Message"]
+        .str.replace("/", " ")
+        .str.replace("-", " ")
+        .apply(contractions.fix)
+        .str.lower()
+        .str.split(" ")
+    )
+
+    df["tokens"] = (
+        df["tokens"]
+        .apply(
+            lambda tokens: [
+                x.translate(str.maketrans({key: None for key in string.punctuation}))
+                if x not in emoticons
+                else x
+                for x in tokens
+            ]
+        )
+        .apply(check_for_dots)
+        .apply(lambda tokens: list(filter(lambda token: token != "", tokens)))
+    )
+
     df["lemas"] = df["tokens"].apply(
         lambda tokens: [lemmatizer.lemmatize(token) for token in tokens]
     )
-
+    stop = stopwords.words("english")
+    df["no_stopwords"] = df["lemas"].apply(
+        lambda lema: [item for item in lema if item not in stop]
+    )
     df["joined_lemas"] = df["lemas"].apply(" ".join)
-    df["joined_lemas"] = df["joined_lemas"].str.strip()
+
     return df
