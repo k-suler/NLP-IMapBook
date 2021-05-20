017d82b76b3391738cf5be1ea7da3a89779c5f28
Tara Bosil <50740910+tarabosil@users.noreply.github.com>
2021-05-20 23:03:17 +0200

    Change: kfold and loocv

--- run_models.py
+++ run_models.py
@@ -28,43 +28,50 @@ rm = RandomModel()
 
 
 def custom_features(features, data):
-    X_train, X_test, y_train, y_test = split_train_test(features, x_col="features", y=data["CodePreliminary"])
+    X_train, X_test, y_train, y_test = split_train_test(features, x_col="features", y=data[["CodePreliminary"]])
     X_train = X_train.toarray()
     X_test = X_test.toarray()
-    print(X_train)
     print(f"{BLUE} Starting with Naive Bayes classifier {ENDC}")
     nb = Model("Naive Bayes", MultinomialNB())
     nb.evaluate(X_train, y_train, X_test, y_test)
 
     print(f"{BLUE} Starting with Po Uri {ENDC}")
-    # nb.kfold(data, False)
+    nb.kfold(features, data, 'custom')
+    # nb.loocv(features, data, 'custom')
 
     print(f"{BLUE} Starting with Support Vector Machine classifier {ENDC}")
     svm = Model("Support Vector Machine", SVC(kernel="rbf", gamma="auto", C=1000))
     svm.evaluate(X_train, y_train, X_test, y_test)
 
+    svm.kfold(features, data, 'custom')
+    # svm.loocv(features, data, 'custom')
+
     print(f"{BLUE} Starting with Logistic Regression classifier {ENDC}")
     lg = Model(
             "Support Vector Machine",
-            LogisticRegression(multi_class="multinomial", max_iter=1000, C=1.0 / 0.01),
+            LogisticRegression(multi_class="multinomial", max_iter=100, C=1.0 / 0.01),
         )
     lg.evaluate(X_train, y_train, X_test, y_test)
 
+    lg.kfold(features, data, 'custom')
+    # lg.loocv(features, data, 'custom')
+
     print(f"{BLUE} Starting with Popularity classifier {ENDC}")
-    # pm.evaluate(X_train, y_train, X_test, y_test)
+    pm.evaluate(y_train, y_test)
 
     print(f"{BLUE} Starting with Random classifier {ENDC}")
-    # rm.evaluate(X_train, y_train, X_test, y_test)
+    rm.evaluate(y_train, y_test)
 
-def bow(data, X_train, X_test, y_train, y_test):
-    # word level TF-IDF
+
+def bow(data):
+    X_train, X_test, y_train, y_test = split_train_test(data, x_col="features", y=data[["CodePreliminary"]])
     X_train, X_test = bag_of_words_features_1(X_train, X_test, kfold=False)
 
     print(f"{BLUE} Starting with Naive Bayes classifier {ENDC}")
     nb = Model("Naive Bayes", MultinomialNB())
     nb.evaluate(X_train, y_train, X_test, y_test)
 
-    # nb.kfold(data, False)
+    nb.kfold(None, data, 'bow')
     # nb.loocv(data, False)
 
     print(f"{BLUE} Starting with Support Vector Machine classifier {ENDC}")
@@ -81,7 +88,7 @@ def bow(data, X_train, X_test, y_train, y_test):
     svm = Model("Support Vector Machine", SVC(kernel="rbf", gamma="auto", C=1000))
     svm.evaluate(X_train, y_train, X_test, y_test)
 
-    # svm.kfold(data, False)
+    svm.kfold(None, data, 'bow')
     # svm.loocv(data, False)
 
     print(f"{BLUE} Starting with Logistic Regression classifier {ENDC}")
@@ -91,14 +98,14 @@ def bow(data, X_train, X_test, y_train, y_test):
     )
     lg.evaluate(X_train, y_train, X_test, y_test)
 
-    # lg.kfold(data, False)
+    lg.kfold(None, data, 'bow')
     # lg.loocv(data, False)
 
     print(f"{BLUE} Starting with Popularity classifier {ENDC}")
-    pm.evaluate(X_train, y_train, X_test, y_test)
+    pm.evaluate(y_train, y_test)
 
     print(f"{BLUE} Starting with Random classifier {ENDC}")
-    rm.evaluate(X_train, y_train, X_test, y_test)
+    rm.evaluate(y_train, y_test)
 
     # print(f"{BLUE} Starting with basic nn classifier {ENDC}")
     # lb = LabelEncoder()
@@ -123,15 +130,15 @@ def bow(data, X_train, X_test, y_train, y_test):
     # nn.evaluate(X_test, Y_test)
 
 
-def tfidf(data, X_train, X_test, y_train, y_test):
-    # word level TF-IDF
+def tfidf(data):
+    X_train, X_test, y_train, y_test = split_train_test(data, x_col="features", y=data[["CodePreliminary"]])
     X_train, X_test = tfidf_features_1(X_train, X_test, kfold=False)
 
     print(f"{BLUE} Starting with Naive Bayes classifier {ENDC}")
     nb = Model("Naive Bayes", MultinomialNB())
     nb.evaluate(X_train, y_train, X_test, y_test)
 
-    # nb.kfold(data, False)
+    nb.kfold(None, data, 'tfidf')
     # nb.loocv(data, False)
 
     print(f"{BLUE} Starting with Support Vector Machine classifier {ENDC}")
@@ -150,7 +157,7 @@ def tfidf(data, X_train, X_test, y_train, y_test):
     )
     svm.evaluate(X_train, y_train, X_test, y_test)
 
-    # svm.kfold(data, False)
+    svm.kfold(None, data, 'tfidf')
     # svm.loocv(data, False)
 
     print(f"{BLUE} Starting with Logistic Regression classifier {ENDC}")
@@ -160,14 +167,14 @@ def tfidf(data, X_train, X_test, y_train, y_test):
     )
     svm.evaluate(X_train, y_train, X_test, y_test)
 
-    # svm.kfold(data, False)
+    svm.kfold(None, data, 'tfidf')
     # svm.loocv(data, False)
 
     print(f"{BLUE} Starting with Popularity classifier {ENDC}")
-    pm.evaluate(X_train, y_train, X_test, y_test)
+    pm.evaluate(y_train, y_test)
 
     print(f"{BLUE} Starting with Random classifier {ENDC}")
-    rm.evaluate(X_train, y_train, X_test, y_test)
+    rm.evaluate(y_train, y_test)
 
     # print(f"{BLUE} Starting with basic nn classifier {ENDC}")
     # lb = LabelEncoder()
@@ -196,17 +203,17 @@ if __name__ == "__main__":
     data = preprocess_data()
     features = custom_features_extractor(data)
 
-    # print(f"{BLUE} ############################ {ENDC}")
-    # print(f"{BLUE} #          TF-IDF          # {ENDC}")
-    # print(f"{BLUE} ############################ {ENDC}")
-    # tfidf(data)
-    #
-    # print(f"{BLUE} ############################ {ENDC}")
-    # print(f"{BLUE} #            BOW           # {ENDC}")
-    # print(f"{BLUE} ############################ {ENDC}")
-    # bow(data)
-
     print(f"{BLUE} ############################ {ENDC}")
     print(f"{BLUE} #          TF-IDF          # {ENDC}")
+    print(f"{BLUE} ############################ {ENDC}")
+    tfidf(data)
+
+    print(f"{BLUE} ############################ {ENDC}")
+    print(f"{BLUE} #            BOW           # {ENDC}")
+    print(f"{BLUE} ############################ {ENDC}")
+    bow(data)
+
+    print(f"{BLUE} ############################ {ENDC}")
+    print(f"{BLUE} #          CUSTOM          # {ENDC}")
     print(f"{BLUE} ############################ {ENDC}")
     custom_features(features, data)
